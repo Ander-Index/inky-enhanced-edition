cc88eff90a668792a126d70aebc5edf3617bf02e
Ander Index <51054841+Ander-Index@users.noreply.github.com>
2026-05-28 17:54:02 +0800

    修复启动速度，并更改app名称

--- app/main-process/i18n/i18n.js
+++ app/main-process/i18n/i18n.js
@@ -1,40 +1,49 @@
-const electron = require('electron');
-const fs = require('fs');
-const path = require('path');
+const electron = require("electron");
+const fs = require("fs");
+const path = require("path");
 
 class i18n {
-    constructor() {
-        this.currentLocale = null;
-        this.msgs = {}
+  constructor() {
+    this.currentLocale = null;
+    this.msgs = {};
 
-        electron.app.on('ready', () => {
-            this.switch(electron.app.getLocale());
-        });
+    electron.app.on("ready", () => {
+      this.switch(electron.app.getLocale());
+    });
 
-        electron.ipcMain.on('i18n._', (event, msgid) => {
-            event.returnValue = this._(msgid);
-        });
+    electron.ipcMain.on("i18n._", (event, msgid) => {
+      event.returnValue = this._(msgid);
+    });
+  }
+
+  _(msgid) {
+    // Strip leading & (Windows accelerator prefix) for lookup
+    var cleanId = msgid.replace(/^&/, "");
+    if (!(cleanId in this.msgs) || !this.msgs[cleanId].length) {
+      this.msgs[cleanId] = cleanId;
     }
+    return this.msgs[cleanId];
+  }
 
-    _(msgid) {
-        if (!(msgid in this.msgs) || !this.msgs[msgid].length) {
-            this.msgs[msgid] = msgid;
+  switch(lang) {
+    this.currentLocale = lang;
+    const file = path.join(__dirname, `${lang}.json`);
+    if (fs.existsSync(file)) {
+      this.msgs = require(file);
+    } else {
+      // Try short locale code (e.g. "zh" from "zh-Hans-CN")
+      const shortLang = lang.split("-")[0];
+      const shortFile = path.join(__dirname, `${shortLang}.json`);
+      if (fs.existsSync(shortFile)) {
+        this.msgs = require(shortFile);
+      } else {
+        const defaultLocale = electron.app.getLocale();
+        if (lang != defaultLocale) {
+          this.switch(defaultLocale);
         }
-        return this.msgs[msgid];
+      }
     }
-
-    switch(lang) {
-        this.currentLocale = lang;
-        const file = path.join(__dirname, `${lang}.json`)
-        if (fs.existsSync(file)) {
-            this.msgs = require(file);
-        } else {
-            const defaultLocale = electron.app.getLocale();
-            if (lang != defaultLocale) {
-                this.switch(defaultLocale);
-            }
-        }
-    }
+  }
 }
 
 module.exports = new i18n();
